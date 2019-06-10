f94c0c88346bbac636900d26a29e6cae59833e73
Brittany Alleman <brittany.alleman1@gmail.com>
2019-06-10 01:15:47 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -8,9 +8,10 @@ def hello_t(array)
 
   array
    puts  "Hey! No block was given!"
-  end
-
+ end
+end
 hello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
+ end
 end
