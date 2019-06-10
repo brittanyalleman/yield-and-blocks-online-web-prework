c677cd2efc2c064a038fec54246e63c4771ac25d
Brittany Alleman <brittany.alleman1@gmail.com>
2019-06-10 01:24:30 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,10 +1,3 @@
-User avatar
-Austin Melchior 6 MINUTES AGO
-Hi Brittany, hows it going?
-Brittany Alleman 6 MINUTES AGO
-Going good! i have a couple errors i need help with
-User avatar
-Brittany Alleman 6 MINUTES AGO
 def hello_t(array)
   i = 0
 
@@ -13,44 +6,8 @@ def hello_t(array)
     i = i + 1
   end
 
-  array
-   puts  "Hey! No block was given!"
- end
-end
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
- end
-end
-User avatar
-Brittany Alleman 5 MINUTES AGO
-/home/wise-command-6085/yield-and-blocks-online-web-prework/lib/hello.rb:12: syntax error, unexpected end, expecting end-of-input
-# ./spec/spec_helper.rb:1:in `require_relative'
-# ./spec/spec_helper.rb:1:in `<top (required)>'
-# ./spec/hello_spec.rb:1:in `<top (required)>'
-No examples found.
-No examples found.
-Finished in 0.00056 seconds (files took 0.17415 seconds to load)
-0 examples, 0 failures, 2 errors occurred outside of examples
-Finished in 0.00056 seconds (files took 0.17415 seconds to load)
-0 examples, 0 failures, 2 errors occurred outside of examples
-[01:15:59] (master) yield-and-blocks-online-web-prework
-// ♥
-lib/hello.rb18:1(17, 246)
-LFUTF-8Ruby Learn
-User avatar
-User avatar
-Austin Melchior 2 MINUTES AGO
-def hello_t(array)
-  i = 0
-
-  while i < array.length
-    yield array[i]
-    i = i + 1
-  end
-
-  array
-   puts  "Hey! No block was given!"
+  array   
+  puts  "Hey! No block was given!"
 
 hello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
@@ -58,3 +15,4 @@ hello_t(["Tim", "Tom", "Jim"]) do |name|
  end
 end
 end
+
